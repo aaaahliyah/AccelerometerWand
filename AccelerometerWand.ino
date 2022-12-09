97ca8df2434d5cb0350bf912b49d90b6ada51fc8
Aaliyah Evans <89546222+aaaahliyah@users.noreply.github.com>
2022-12-09 14:10:28 -0600

    added buttons, removed while (!serial)
    added button functions that work with the game, removed need for serial monitor to be open for program to run

--- AccelerometerWand.ino
+++ AccelerometerWand.ino
@@ -1,35 +1,48 @@
 /* ============================================
 
-   Notes for connection to the Teensy
+   Notes for MPU 6050 Accelerometer connection to the Teensy
    MPU 6050     Teensy
-   Vcc          3.3V
+   VCC          3.3V
    GND          GND
-   SCL          pin A5
-   SDA          pin A4
-   XDA          NC  (no connection)
-   XCL          NC  (no connection)
-   ADO          NC
-   INT          pin 2
+   SCL          pin 19
+   SDA          pin 18
+
+   Notes for Other Wires
+   Top Button       pin 17
+   Middle Button    pin 16
+   Bottom Button    pin 15
+   
 
 ============================================ */
 
 #include "Wire.h"
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <Bounce.h>
 
 MPU6050 accelgyro;
 unsigned long time;
 int16_t ax, ay, az;
 float heading;
 
+// buttons
+const int topButtonPin = 17;
+const int middleButtonPin = 16;
+const int bottomButtonPin = 15;
 
+// using bounce to detect chatter from buttons and detect changes easily
+Bounce topButton = Bounce(17, 10);
+Bounce middleButton = Bounce(16, 10);  
+Bounce bottomButton = Bounce(15, 10);
 
 void setup() {
 
+  // removed the while !(serial) from example code as the sketch would not run without serial monitor open
+ 
   // accelerometer setup
   Wire.begin();   // join I2C bus (I2Cdev library doesn't do this automatically)
   Serial.begin(115200); // Set baud rate to 115200 (figure out ways to double it)
-  while (!Serial);
+  
 
   Serial.println("Initializing I2C devices...");
   delay(500);
@@ -44,18 +57,29 @@ void setup() {
 
 
   // button(s) setup
+  pinMode(topButtonPin, INPUT);
+  pinMode(middleButtonPin, INPUT);
+  pinMode(bottomButtonPin, INPUT);
   
+  digitalWrite(topButtonPin, LOW);
+  digitalWrite(middleButtonPin, LOW);
+  digitalWrite(bottomButtonPin, LOW);
+
+
 }
 
 
 void loop() {
-  // these methods (and a few others) are also available
+
+  // fetches the acceleration from the accelerometer with a method from the I2Cdev library
   accelgyro.getAcceleration(&ax, &ay, &az);
-  
+
+  // squaring the acceleration values returned by .getAcceleration()
   float accelerationX = (ax * ax);
   float accelerationY = (ay * ay);
   float accelerationZ = (az * az);
 
+  // calculating the total raw acceleration from the previous methods + remapping it to work with joystick axis values
   float rawAcceleration = sqrt(accelerationX + accelerationY + accelerationZ);
   float remappedAcceleration = map(rawAcceleration, 15000, 48000, 512, 1024);
   Serial.println("Raw Acceleration: "); 
@@ -66,10 +90,41 @@ void loop() {
   delay(100); // run at ~100 Hz
 
 
-   /* Basic USB Joystick Example
-   Teensy becomes a USB joystick
+  // buttons
 
-   You must select Joystick from the "Tools > USB Type" menu
+  topButton.update();
+  middleButton.update();
+  bottomButton.update();
+
+// top button - controls left movement (for selection screens)
+  if (topButton.fallingEdge())
+  {
+    Keyboard.print("a");
+    Serial.println("The top button was pressed.");
+    delay(20);
+  }
+
+
+// middle button - acts as select button
+  if (middleButton.fallingEdge())
+  {
+    Keyboard.print("s");
+    Serial.println("The middle button was pressed.");
+    delay(20);
+  }
+
+// bottom button - controls right movement (for selection screens)
+ if (bottomButton.fallingEdge())
+  {
+    Keyboard.print("d");
+    Serial.println("The bottom button was pressed.");
+    delay(20);
+  }
+
+
+   /*
+
+   You must select Joystick from the "Tools > USB Type" menu. Use the Serial + Keyboard + Mouse + Joystick WITH the serial monitor OPEN as this is the only way I've found it to work.
 
    */
 
